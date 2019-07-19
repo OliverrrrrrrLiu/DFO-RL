abbb0d8eafc66188180d5ac4db8fdc1e3b016e0d
OliverrrrrrrLiu <zhuoran.o.liu@gmail.com>
2019-07-18 23:41:46 -0400

    Better-factorized ECNoise and FD

--- FD.py
+++ FD.py
@@ -1,30 +1,31 @@
 import numpy as np
 from numpy.core.umath_tests import inner1d
 
+# TODO: implement second-order finite difference table when mu_2 is not found
+
 class MaxHessian(object):
     """Estimate the maximum absolute Hessian entry for function f"""
-    def __init__(self, f, eps, tau_1 = 100, tau_2 = 0.1):
+    def __init__(self, eps, tau_1 = 100, tau_2 = 0.1):
         """
         @param f:         noisy function to be evaluated
         @param eps:       estimated noise level
         @param tau_i:     constant used for Hessian estimation, see More and Wild "Estimating Derivatives of Noisy Simulations", 2012
         """
-        self.f = f
         self.eps = eps
         self.tau_1 = tau_1
         self.tau_2 = tau_2
 
-    def calculate_delta(self, x, h, direction = None):
+    def calculate_delta(self, f, x, h, direction = None):
         """Calculate the second-order difference"""
-        fx = self.f(x)
+        fx = f(x)
         if isinstance(x, (list, np.ndarray)):
             x = np.array(x)
             d = direction if direction is not None else np.random.choice(len(x))
             tmp = np.zeros(len(x))
             tmp[d] = h
             h = tmp
-        fx_plus_h = self.f(x + h)
-        fx_minus_h = self.f(x - h)
+        fx_plus_h = f(x + h)
+        fx_minus_h = f(x - h)
         delta = abs(fx_plus_h + fx_minus_h - 2 * fx)
         return delta, fx, fx_plus_h, fx_minus_h
 
@@ -37,30 +38,30 @@ class MaxHessian(object):
         @param fx_minus_h: function value evaluated at f(x-h)
         """
         c1 = dh / self.eps >= self.tau_1
-        c2 = abs(fx_plus_h - fx)  <= self.tau_2 * max(fx, fx_plus_h) #TODO vectorize
+        c2 = abs(fx_plus_h - fx)  <= self.tau_2 * max(fx, fx_plus_h)
         c3 = abs(fx_minus_h - fx) <= self.tau_2 * max(fx, fx_minus_h)
         return c1 and c2 and c3
 
-    def calculate_mu(self, x, mu = 1, direction = None):
+    def calculate_mu(self, f, x, mu = 1, direction = None):
         """Calculate the Hessian estimate
         @param mu: previous estimate of Hessian, to be used as a scalar (default: 1)
         """
         h = (self.eps / mu) ** 0.25
-        dh, fx, fx_plus_h, fx_minus_h = self.calculate_delta(x, h, direction)
+        dh, fx, fx_plus_h, fx_minus_h = self.calculate_delta(f, x, h, direction)
         mu = dh / h ** 2
         return h, dh, mu, fx, fx_plus_h, fx_minus_h
 
-    def estimate(self, x, direction = None):
+    def estimate(self, f, x, direction = None):
         """Estimate the max absolute Hessian
         @param x:         number or list type. Input value
         @param direction: nonnegative integer or None. Direction with respect to
                           which the finite-difference Hessian estimation will be
                           conducted (default: None, random direction)
         """
-        h_a, dh_a, mu_a, fx, fx_plus_h_a, fx_minus_h_a = self.calculate_mu(x, direction = direction)
+        h_a, dh_a, mu_a, fx, fx_plus_h_a, fx_minus_h_a = self.calculate_mu(f, x, direction = direction)
         if self.exit(h_a, dh_a, fx, fx_plus_h_a, fx_minus_h_a):
             return mu_a
-        h_b, dh_b, mu_b, fx, fx_plus_h_b, fx_minus_h_b = self.calculate_mu(x, mu = mu_a, direction = direction)
+        h_b, dh_b, mu_b, fx, fx_plus_h_b, fx_minus_h_b = self.calculate_mu(f, x, mu = mu_a, direction = direction)
         if self.exit(h_b, dh_b, fx, fx_plus_h_b, fx_minus_h_b) or abs(mu_a - mu_b) <= mu_b / 2:
             return mu_b
         else:
@@ -73,7 +74,7 @@ def f(x):
     return inner1d(x, x) + x[:,0] ** 2 + np.random.normal(0, 0.01, n)
 
 def fd_gradient(f, x, eps,h = None, mode = "fd", tau_1 = 100, tau_2 = 0.1):
-    mu = MaxHessian(f, eps, tau_1, tau_2).estimate(x, direction = None)
+    mu = MaxHessian(eps, tau_1, tau_2).estimate(f, x, direction = None)
     dim = len(x)
     if mu is None: return None
     if mode == "fd":
@@ -90,12 +91,12 @@ def fd_gradient(f, x, eps,h = None, mode = "fd", tau_1 = 100, tau_2 = 0.1):
         h_mat[np.arange(dim), np.arange(dim)] = h
         f_incr, f_decr = f(x + h_mat), f(x - h_mat)
         h *= 2
-    return (f_incr - f_decr) / h
+    return (f_incr - f_decr) / h, h
 
 if __name__ == "__main__":
     lst = []
     for _ in range(1000):
-        tmp = fd_gradient(f, np.array([5, 1]), 0.01)
+        tmp, _ = fd_gradient(f, np.array([5, 1]), 0.01)
         if tmp is not None: lst.append(tmp)
     lst = np.array(lst)
     print(np.mean(lst, axis=0))
